9f7e9d9b565d0fcbdd9ca6ce9afe8c565459d4da
damade <damilolaadeoye545@yahoo.com>
2020-05-20 22:07:59 +0100

    commented it

--- GUI/guiPasswordChecker.py
+++ GUI/guiPasswordChecker.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
-import CheckMyPassForGui as passwordCheckerModule;
+
+import CheckMyPassForGui as passwordCheckerModule
 
 
 class App(tk.Frame):
@@ -9,61 +10,64 @@ class App(tk.Frame):
         self.pack()
 
 
-def printPasswordResult():
+def printPasswordResult():  # The function that prints out the result of what is parsed
     inputForPassword = passwordInput.get()
     outputForPasswordChecker = passwordCheckerModule.main(inputForPassword)
     for output in outputForPasswordChecker:
-        mylist.insert(END,str(output)+"\n")
+        mylist.insert(END, str(output) + "\n")
     return
 
-def clearTextField():
-    passwordInput.delete(0,'end')
-    mylist.delete(0,'end')
+
+def clearTextField():  # It is the function that clears the entry and result list box
+    passwordInput.delete(0, 'end')
+    mylist.delete(0, 'end')
     return
 
-def showPassword():
-    passwordInput.show
-# create the application
+
+# create the application frame though the root Tkinter Object
 myapp = App()
 top = Frame(myapp)
 bottom = Frame(myapp)
 listBottom = Frame(myapp)
-# passwordChecker = CheckMyPassForGui()
 
+# create the scrollbar for the app
 scrollbar = Scrollbar(myapp)
 scrollbar.pack(side="right", fill="y")
 
-listScrollBar = Scrollbar(listBottom,orient="vertical")
-listScrollBar.pack(side="right",fill="y")
+# create the scrollbar for the listbox
+listScrollBar = Scrollbar(listBottom, orient="vertical")
+listScrollBar.pack(side="right", fill="y")
 
+# creating objects that would stack at the top part of the frame
 top.pack()
+
+# Create the label
 passwordLabel = Label(top, text="Enter Password")
 passwordLabel.pack(side="left", pady=(20, 0), padx=(4, 4))
 
+# create the input or entry for data
 passwordInput = Entry(top, bd=5, width=150)
 passwordInput.pack(side="right", pady=(20, 0))
 
-#showButton = Button(top,text="show",command= showPassword)
-# here are method calls to the window manager class'''
-
-
+# creating objects that would stack at the middle part of the frame
 bottom.pack()
+
+# create the button for result
 getResult = Button(bottom, text="Check Password", command=printPasswordResult, justify="center", width="15")
-getResult.pack(side="left", pady=(20, 0), padx=(10,40))
+getResult.pack(side="left", pady=(20, 0), padx=(10, 40))
 
-clearButton = Button(bottom, text="Clear",command= clearTextField,justify="center",width="15")
-clearButton.pack(side="right", pady=(20, 0), padx=(30,20))
+# create the button to clear screen
+clearButton = Button(bottom, text="Clear", command=clearTextField, justify="center", width="15")
+clearButton.pack(side="right", pady=(20, 0), padx=(30, 20))
 
-
-
+##creating objects that would stack at the bottom part of the frame
 listBottom.pack(side="bottom", fill="both")
+
+# creating the output listbox for output
 mylist = Listbox(listBottom)
-mylist.config( yscrollcommand=listScrollBar.set)
+mylist.config(yscrollcommand=listScrollBar.set)
 mylist.pack(side="bottom", fill="both", padx=(10, 10), pady=(20, 10))
 listScrollBar.config(command=mylist.yview)
-
-
-
 
 # Gets the requested values of the height and widht.
 windowWidth = myapp.winfo_reqwidth()
@@ -73,11 +77,23 @@ windowHeight = myapp.winfo_reqheight()
 positionRight = int(myapp.winfo_screenwidth() / 2 - windowWidth / 2)
 positionDown = int(myapp.winfo_screenheight() / 2 - windowHeight / 2)
 
-# Positions the window in the center of the page.
-myapp.master.geometry("+{}+{}".format(positionRight, positionDown))
-myapp.master.resizable(0,0)
-myapp.master.title("PassWord Checker")
-myapp.master.maxsize(400, 500)
-myapp.master.iconphoto(False,tk.PhotoImage(file='iconPassword.png'))
-# start the program
-myapp.mainloop()
+if __name__ == "__main__":
+    # here are method calls to the window manager class
+
+    # Positions the window in the center of the page.
+    myapp.master.geometry("+{}+{}".format(positionRight, positionDown))
+
+    # it hides the maximize button on the window
+    myapp.master.resizable(0, 0)
+
+    # it sets title to the windows panel class
+    myapp.master.title("PassWord Checker")
+
+    # set the maximum dimension of the frame
+    myapp.master.maxsize(400, 500)
+
+    # stting the icon for the window frame
+    myapp.master.iconphoto(False, tk.PhotoImage(file='iconPassword.png'))
+
+    # start the program
+    myapp.mainloop()
